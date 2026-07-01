33b46633dcb17fe7224b27a8bca1afdfff351681
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-07-01 03:40:28 -0400

    render the about page and comtact page

--- django_1/django_1/views.py
+++ django_1/django_1/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse  #now we are ready for response and request both 
 from django.shortcuts import render
-#now we are ready for response and request both 
+
 
 # now we have to make some meathods or we can say functions 
 
@@ -8,12 +8,14 @@ def home(request): # means ek request pe mujhe is home to return karna hai
     # return HttpResponse("this is the home page")
     return render(request,'websites/index.html')
 
-def about(request): # means ek request pe mujhe is home to return karna hai 
-    return HttpResponse("this is the about page") 
+def about(request): # means ek request pe mujhe is about ko return karna hai 
+    # return HttpResponse("this is the about page") 
+    return render(request , 'websites/about.html')
 
 
-def contact(request): # means ek request pe mujhe is home to return karna hai 
-    return HttpResponse("this is the contact page") 
+def contact(request): # means ek request pe mujhe iss contatc  ko  return karna hai 
+    # return HttpResponse("this is the contact page") 
+     return render(request , 'websites/contact.html')
 
 #now we have to handle routes from url.py .. bcs export vagera inta nhi hota python mein 
 
